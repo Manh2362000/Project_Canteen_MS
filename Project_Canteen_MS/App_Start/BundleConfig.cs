7bad318a05c09f0f71cd9b714f873c9112f3a0cc
Huy Quang <truongquanghuybx0914@gmail.com>
2021-10-20 14:10:01 +0700

    Add project files.

---
+++ Project_Canteen_MS/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Project_Canteen_MS
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+            // add css theme
+            bundles.Add(new StyleBundle("~/canteen/css").Include(
+                      "~/Content/canteen/css/styles.css"
+                ));
+            // add js theme
+            bundles.Add(new ScriptBundle("~/canteen/js").Include(
+                        "~/Content/canteen/js/datatables-simple-demo.js",
+                        "~/Content/canteen/js/litepicker.js",
+                        "~/Content/canteen/js/scripts.js"
+                ));
+        }
+    }
+}
---
+++ Project_Canteen_MS/Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace Project_Canteen_MS.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("CANTEEN")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public System.Data.Entity.DbSet<Project_Canteen_MS.Models.Staff> Staffs { get; set; }
+
+        public System.Data.Entity.DbSet<Project_Canteen_MS.Models.Bill> Bills { get; set; }
+    }
+}
